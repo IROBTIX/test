bbe7b6b45af571c55c0faa20bf2b8655127132bc
Alexandra Tirlea <alexandra.tirlea@raiffeisen.ro>
2025-07-14 23:48:30 +0300

    third commit

--- app/src/main/java/com/example/mytestapplication/MainActivity.kt
+++ app/src/main/java/com/example/mytestapplication/MainActivity.kt
@@ -12,11 +12,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mytestapplication.ui.theme.MyTestApplicationTheme
-import java.util.Date                        //  This import is unused
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(null) // lint warning 
+        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MyTestApplicationTheme {
