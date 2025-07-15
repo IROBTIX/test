36cab3f54b33aaa841e36cc16301906176feea9b
Alexandra Tirlea <alexandra.tirlea@raiffeisen.ro>
2025-07-15 15:11:43 +0300

    another commit @

--- mylibrary/src/main/java/com/example/mylibrary/MyLibrary.kt
+++ mylibrary/src/main/java/com/example/mylibrary/MyLibrary.kt
@@ -1,6 +1,7 @@
 package com.example.mylibrary
 
-import java.util.Currency // unused import
+import android.util.Log
+
 /**
  * Created by Alexandra Tirlea on 15.07.2025.
  * Raiffeisen Bank RO
@@ -10,8 +11,25 @@ class MyLibrary {
         return "Hello from MyLibrary!"
     }
 
+    private fun veryLongMethod(
+        a: String? = null,
+        b: String? = null,
+        c: String? = null,
+        d: String? = null,
+        e: String? = null,
+        f: String? = null,
+        g: String? = null,
+        h: String? = null,
+        i: String? = null,
+        j: String? = null,
+    ) {
+
+        Log.e("X_TAG", "msg")
+    }
+
     fun main() {
         val myLibrary = MyLibrary()
+        veryLongMethod()
         println(myLibrary.getGreeting())
     }
 }
